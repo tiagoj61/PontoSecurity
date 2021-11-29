81ecb39fa6d005094b572ced0a22088bb2b5555f
Tiago Moraes <tiagoj61@gmail.com>
2021-11-29 05:44:17 -0300

    feat: add ble communicate

--- app/src/main/java/com/example/pontosecurity/communicate/ComunicateToArduino.java
+++ app/src/main/java/com/example/pontosecurity/communicate/ComunicateToArduino.java
@@ -1,7 +1,21 @@
 package com.example.pontosecurity.communicate;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+
 public class ComunicateToArduino {
     public static void sendAlertToBLE(){
 
+    }
+    private void connect() {
+//        try {
+//            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+//            BluetoothDevice device = bluetoothAdapter.getRemoteDevice(deviceAddress);
+//            SerialSocket socket = new SerialSocket(getActivity().getApplicationContext(), device);
+//            service.connect(socket);
+
+//        } catch (Exception e) {
+//            onSerialConnectError(e);
+//        }
     }
 }
