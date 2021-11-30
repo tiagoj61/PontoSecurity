71933bc973712f3793c03c0a9f0ab6d0e59b5c17
Tiago Moraes <tiagoj61@gmail.com>
2021-11-30 07:57:53 -0300

    feat: add post to server

--- app/src/main/java/com/example/pontosecurity/communicate/ComunicateToServer.java
+++ app/src/main/java/com/example/pontosecurity/communicate/ComunicateToServer.java
@@ -1,9 +1,77 @@
 package com.example.pontosecurity.communicate;
 
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.Year;
+
 public class ComunicateToServer {
     public static boolean pushPassToServer(String senha){
 
 
         return true;
+    }
+
+    private void doPost(){
+        HttpURLConnection connection = null;
+        try {
+            URL url = new URL("");
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("charset", "utf-8");
+
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+
+            JSONObject body = new JSONObject();
+            body.put("container", "");
+
+            String jsonInputString = body.toString();
+            try (OutputStream os = connection.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes("utf-8");
+                os.write(input, 0, input.length);
+                os.close();
+            }
+
+            InputStream in = new BufferedInputStream(connection.getInputStream());
+            String resultado = IOUtils.toString(in, "UTF-8");
+
+            JSONObject result = new JSONObject(resultado);
+
+            JSONArray responseFiles = new JSONArray(result.get("blobs").toString());
+            for (int i = 0; i < responseFiles.length(); i++) {
+
+                JSONObject file = new JSONObject(responseFiles.get(i).toString());
+                String nomeArquivo = file.getString("name");
+                String caminhoUrl = file.getString("url");
+                //String caminhoUrl = file.getString("size");
+
+            }
+
+            JSONObject response = new JSONObject(resultado);
+
+            int statusCode = connection.getResponseCode();
+
+            System.out.println("Response code S3 Files Origins: " + statusCode);
+
+            in.close();
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+
     }
 }
